bc2025c2ce922dfef3ce1419adef3367930b7a05
ROKAS GARMASUKIS <ROKAS.GARMASUKIS@GMAIL.COM>
2022-03-15 22:58:31 +0100

    Add events#edit and events#show functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  include ApplicationHelper
   before_action :require_login, except: :index
 
   def index
@@ -10,14 +11,33 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new(event_params)
-    @event.creator_id = session[:user_id]
+    @event = current_user.created_events.build(event_params)
 
     if @event.save
       redirect_to root_path, notice: "Event created successfully"
     else
       flash.now[:alert] = "Please check input"
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    puts params[:id]
+    @event = Event.find_by(id: params[:id])
+  end
+
+  def edit
+    @event = Event.find_by(id: params[:id])
+  end
+
+  def update
+    @event = Event.find_by(id: params[:id])
+
+    if @event.update(event_params)
+      redirect_to event_path, notice: "Event updated successfully"
+    else
+      flash.now[:alert] = "Please check input"
+      render :edit, status: :unprocessable_entity
     end
   end
 
