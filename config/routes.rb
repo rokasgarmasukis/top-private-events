bda4fc37b06090c78c778a508dcb88e9e09c435c
ROKAS GARMASUKIS <ROKAS.GARMASUKIS@GMAIL.COM>
2022-03-15 21:26:52 +0100

    Add Boostrap, add flash and navbar partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "events#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "events#index"
 end
