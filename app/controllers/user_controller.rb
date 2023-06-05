49dfbbffd5711d7ac75b65c3422be654bf1e3f8e
ROKAS GARMASUKIS <ROKAS.GARMASUKIS@GMAIL.COM>
2023-06-05 13:37:12 +0200

    Finish navbar and card styling

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,7 @@ class UserController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @events = current_user.created_events
+    @created_events = current_user.created_events
+    @attended_events = current_user.attended_events
   end
 end
